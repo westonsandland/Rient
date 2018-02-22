9d43e24b24fc5747d476d5070d44cdf26bfb9bff
Sandland, Weston <ws6283@lib-pclem036.lib.utexas.edu>
2018-02-22 17:03:05 -0600

    Initial Commit

---
+++ LocationsAlpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationsAlpha
+//
+//  Created by Sandland, Weston on 2/22/18.
+//  Copyright © 2018 Sandland, Weston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

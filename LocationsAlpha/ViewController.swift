16921a2b35e246c3e75c9a2d3c59ce95e7be2e1a
Sandland, Weston <ws6283@lib-pclem036.lib.utexas.edu>
2018-02-22 18:29:09 -0600

    second qq

--- LocationsAlpha/ViewController.swift
+++ LocationsAlpha/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var Tower: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways
+        {
+            Tower.text = String(describing: locationManager.location?.coordinate.latitude)
+            Tower.text = String(describing: locationManager.location?.coordinate.longitude)
+        }
+        // Tower.frame.origin = CGPoint(x: <#T##Int#>, y: <#T##Int#>)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

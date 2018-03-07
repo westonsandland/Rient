d0ab6358876230ef209aaf3c3f9d6aa49d9ba46d
Sandland, Weston <ws6283@lib-pclem040.lib.utexas.edu>
2018-03-06 19:30:18 -0600

    version 1.0 - this version is the app store submission

--- LocationsAlpha/ViewController.swift
+++ LocationsAlpha/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, URLSessionDel
     @IBOutlet weak var DistanceFrom: UILabel!
     @IBOutlet weak var DestinationLabel: UILabel!
     @IBOutlet weak var blackout: UIImageView!
+    @IBOutlet weak var arrived: UILabel!
     
     var destinationEntry: String = "none"
     var destinationLatitude: Double = 0.0
@@ -31,6 +32,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, URLSessionDel
     var correction: Double = 0
     var fullAddress: [String] = [""]
     var webData: Data?
+    var exactDistanceFrom: Double = 0.0
+    var isHere: Bool = false
     
     override func viewWillAppear(_ animated: Bool) {
         UIApplication.shared.statusBarStyle = .default
@@ -86,8 +89,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, URLSessionDel
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let fromTowerTuple = getLatitudeAndLongitudeFromTower()
-        LatitudeFrom.text = String(describing: round(fromTowerTuple.latitude*100000)/100000)
-        LongitudeFrom.text = String(describing: round(fromTowerTuple.longitude*100000)/100000)
+        LatitudeFrom.text = String(describing: round(fromTowerTuple.latitude*10000000)/10000000)
+        LongitudeFrom.text = String(describing: round(fromTowerTuple.longitude*10000000)/10000000)
         let targetLocation = CLLocation(latitude: destinationLatitude, longitude: destinationLongitude)
         let myLocation = CLLocation(latitude: (locationObj.location?.coordinate.latitude)!, longitude: (locationObj.location?.coordinate.longitude)!)
         //print("TARGET LAT AND LONG IS:")
@@ -96,9 +99,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate, URLSessionDel
         //print("MY LAT AND LONG IS:")
         //print(myLocation.coordinate.latitude)
         //print(myLocation.coordinate.longitude)
-        DistanceFrom.text = String(round(targetLocation.distance(from: myLocation) * 0.000621371 * 100)/100) + " miles away"
+        exactDistanceFrom = targetLocation.distance(from: myLocation) * 0.000621371
+        DistanceFrom.text = String(round(exactDistanceFrom * 100)/100) + " miles away"
         rotatePointer(xyTuple: fromTowerTuple)
         //print("current angle is " + String(currentAngle))
+        hereCheck()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
@@ -154,43 +159,98 @@ class ViewController: UIViewController, CLLocationManagerDelegate, URLSessionDel
         //Tower.frame.origin = CGPoint(x: 0.0, y: 0.0)
     }
     
+    func hereCheck()
+    {
+        if(exactDistanceFrom < 0.005 && !isHere)
+        {
+            animateHere()
+            isHere = true
+        } else if(exactDistanceFrom >= 0.005 && isHere)
+        {
+            removeHere()
+            isHere = false
+        }
+    }
+    
     func animateHere()
     {
+        UIView.animate(withDuration: 1.0, animations:
+        {
+            self.blackout.alpha = 1.0
+            self.arrived.alpha = 1.0
+            self.view.backgroundColor = #colorLiteral(red: 0.80303514, green: 0.2736586928, blue: 0.2775121331, alpha: 1)
+        }, completion: nil)
+    }
+    
+    func removeHere()
+    {
+        UIView.animate(withDuration: 1.0, animations:
+            {
+                self.blackout.alpha = 0.0
+                self.arrived.alpha = 0.0
+                self.view.backgroundColor = #colorLiteral(red: 0.1960576177, green: 0.1960916817, blue: 0.1960501969, alpha: 1)
+        }, completion: nil)
+    }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
         
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
         
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         blackout.alpha = 0.0
+        arrived.alpha = 0.0
         //locationManager.requestAlwaysAuthorization()
         LatitudeFrom.adjustsFontSizeToFitWidth = true
         LongitudeFrom.adjustsFontSizeToFitWidth = true
         DestinationLabel.text = "Directions to \(destinationEntry)"
         enableLocationServices()
-        print(destinationEntry)
+        //print(destinationEntry)
         //constraints start here
         //Pointer.translatesAutoresizingMaskIntoConstraints = false
         //view.addSubview(Pointer)
-        view.addSubview(blackout)
-        let margins = view.layoutMarginsGuide
         //let bounds = UILayoutGuide()
         //let widthMinusPointer = margins.rightAnchor - #imageLiteral(resourceName: "Pointer").size.width
         //let distanceFromLeft = widthMinusPointer / (1.7)
         //let distanceFromRight = widthMinusPointer - distanceFromLeft
         //let leftConstraint = NSLayoutConstraint(item: Pointer, attribute: .leading, relatedBy: .equal, toItem: bounds, attribute: .trailingMargin, multiplier: 1/1.7, constant: #imageLiteral(resourceName: "Pointer").size.width/(-1.7))
         //let rightConstraint = NSLayoutConstraint(item: Pointer, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 2.4/1.7, constant: ((-0.7)/1.7)*#imageLiteral(resourceName: "Pointer").size.width)
-        NSLayoutConstraint.activate([
-            blackout.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
-            blackout.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
-            ])
+        
         //NSLayoutConstraint.activate([leftConstraint, rightConstraint])
         // Do any additional setup after loading the view, typically from a nib.
+//        blackout.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(blackout)
+//        let margins = view.layoutMarginsGuide
+//        NSLayoutConstraint.activate([
+//            blackout.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+//            blackout.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+//            ])
+        //blackout.image = resizeImage(image: blackout.image!, targetSize: self.view.frame.size)
+        //print("constraints updated")
     }
     
-    //override func updateViewConstraints() {
-        /**/
-    //}
     // delegate methods
     /*
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse,
